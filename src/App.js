8fd5696e6cdde5640918beeb89a85f3dd1f31551
Chiranjivee Saksham <75401112+striver-69@users.noreply.github.com>
2021-03-26 11:43:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import YoutubeForm from "./components/YoutubeForm";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <YoutubeForm />
+    </div>
+  );
+}
---
+++ src/components/YoutubeForm.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import TextError from "./TextError";
+
+const initialValues = {
+  name: "Chiru",
+  email: "",
+  channel: "",
+  comments: "",
+  address: "",
+  social: {
+    facebook: "",
+    twitter: ""
+  },
+  phoneNumbers: ["", ""]
+};
+
+const onSubmit = (values) => {
+  console.log(values);
+};
+
+const validationSchema = Yup.object({
+  name: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email format").required("Required"),
+  channel: Yup.string().required("Required")
+});
+
+function YoutubeForm() {
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      <Form>
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <Field type="text" id="name" name="name" />
+          <ErrorMessage name="name" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <Field type="email" id="email" name="email" />
+          <ErrorMessage name="email">
+            {(errorMsg) => <div className="error">{errorMsg}</div>}
+          </ErrorMessage>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <Field type="text" id="channel" name="channel" />
+          <ErrorMessage name="channel" component={TextError} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="comments">Comments</label>
+          <Field as="textarea" id="comments" name="comments" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="address">Address</label>
+          <Field name="address">
+            {(props) => {
+              const { field, form, meta } = props;
+              console.log("Render Props", props);
+              return (
+                <div>
+                  <input type="text" id="address" {...field} />
+                  {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                </div>
+              );
+            }}
+          </Field>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="facebook">Facebook profile</label>
+          <Field type="text" id="facebook" name="social.facebook" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="twitter">Twitter profile</label>
+          <Field type="text" id="twitter" name="social.twitter" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="primaryPh">Primary PhoneNumbers</label>
+          <Field type="text" id="primaryPh" name="phoneNumbers[0]" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="secondaryPh">Primary PhoneNumbers</label>
+          <Field type="text" id="secondaryPh" name="phoneNumbers[1]" />
+        </div>
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  );
+}
+
+export default YoutubeForm;
